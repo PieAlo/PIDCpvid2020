7a35434b5f2dfbabf89dde4c8f15c2d69c41f314
pierodiaz <pierodiazxd@gmail.com>
2020-05-16 15:03:13 -0500

    Agregando 3ra fila

--- ProyectoPId2020/src/demo/inicio/Mensaje.java
+++ ProyectoPId2020/src/demo/inicio/Mensaje.java
@@ -5,6 +5,9 @@ public class Mensaje {
 	public static void main(String[] args) {
 		System.out.println("Proyecto Covid");
 		System.out.println("Proyecto Covid 2");
+		System.out.println("Proyecto Covid 3");
+		
+		/*https://github.com/PieAlo/PIDCpvid2020.git*/
 
 	}
 
